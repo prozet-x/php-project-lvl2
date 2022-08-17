cef9643a9cf64024d2b245d752fe6ee2ba478120
prozet <abcd-xyz@list.ru>
2022-08-17 13:10:35 -0700

    st 6. gendiff bad yet

--- src/gendiff.php
+++ src/gendiff.php
@@ -13,16 +13,6 @@ function getFormattedValue($value)
 
 function getNewDiffElem($key, $changes, $value, ...$args)
 {
-    print_r('___________________' . PHP_EOL);
-    print_r($key);
-    print_r(PHP_EOL);
-    print_r($changes);
-    print_r(PHP_EOL);
-    print_r($value);
-    print_r(PHP_EOL);
-    print_r($args);
-    print_r(PHP_EOL);
-    print_r('___________________' . PHP_EOL);
     $res = ['key' => $key, 'changes' => $changes, 'value' => getFormattedValue($value)];
     return count($args) === 1
         ? [...$res, 'oldValue' => getFormattedValue($args[0])]
@@ -35,15 +25,11 @@ function makeDiff($before, $after)
         return getNewDiffElem($key, $changes, $value, ...$args);
     };
 
-    //$totalData = array_merge($before, $after);
     $keys = array_unique(array_merge(array_keys($before), array_keys($after)));
 
     return array_reduce(
         $keys,
         function ($acc, $key) use ($before, $after, $getNewDiffElemFunc) {
-            print_r($before);
-            print_r($after);
-            print_r($key . PHP_EOL);
             $inBefore = array_key_exists($key, $before);
             $inAfter = array_key_exists($key, $after);
             if (!$inBefore) {
@@ -56,19 +42,11 @@ function makeDiff($before, $after)
                 return [...$acc, $getNewDiffElemFunc($key, 'u', $after[$key], $before[$key])];
             }
             if (!is_array($before[$key]) and !is_array($after[$key])) {
-                print_r('_________________' . PHP_EOL);
-                print_r($before[$key]);
-                print_r(PHP_EOL);
-                print_r($after[$key]);
-                print_r(PHP_EOL);
-                print_r($before[$key] === $after[$key]);
-                print_r(PHP_EOL);
-                print_r('_________________' . PHP_EOL);
                 return $before[$key] === $after[$key]
                        ? [...$acc, $getNewDiffElemFunc($key, 'n', $after[$key])]
                        : [...$acc, $getNewDiffElemFunc($key, 'u', $after[$key], $before[$key])];
             }
-            return [...$acc, $getNewDiffElemFunc($key, 'n', makeDiff($before[$key], $after[$key]))];
+            return [...$acc, ['key' => $key, 'changes' => 'n', 'value' => makeDiff($before[$key], $after[$key])]];
         },
         []
     );
