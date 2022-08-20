98a7e94eba2a0e50436e8d029d5775afcae24de7
prozet <abcd-xyz@list.ru>
2022-08-20 10:10:42 -0700

    st 8 done

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -9,46 +9,38 @@ function getformattedValue($value)
     return trim(json_encode($value), '"');
 }
 
-function getFormattedString($key, $value, $deep, $changesSymbol)
+function getFormattedString($key, $value, $deep, $changesSymbol = ' ')
 {
     return is_array($value)
     ? "  $changesSymbol $key: " . formatStylish($value, $deep + 1)
-    : "  $changesSymbol $key: " . getformattedValue($value) . PHP_EOL;
+    : "  $changesSymbol $key: " . getformattedValue($value);
 }
 
 function formatStylish($diff, $deep = 0)
 {
-    $getFormattedStringFunc = function ($key, $value, $deep, $changesSymbol = ' ') {
-        return getFormattedString($key, $value, $deep, $changesSymbol);
-    };
-
     if (count(array_filter($diff, fn ($elem) => $elem['changes'] !== 'n')) > 0) {
         usort($diff, fn ($a, $b) => strcmp($a['key'], $b['key']));
     }
 
-    return '{' . PHP_EOL
-        . array_reduce(
-            $diff,
-            function ($acc, $elem) use ($deep, $getFormattedStringFunc) {
-                $res = str_repeat(TAB, $deep);
-                switch ($elem['changes']) {
-                    case 'r':
-                        $res .= $getFormattedStringFunc($elem['key'], $elem['value'], $deep, '-');
-                        break;
-                    case 'a':
-                        $res .= $getFormattedStringFunc($elem['key'], $elem['value'], $deep, '+');
-                        break;
-                    case 'u':
-                        $res .= $getFormattedStringFunc($elem['key'], $elem['oldValue'], $deep, '-')
-                        . str_repeat(TAB, $deep)
-                        . $getFormattedStringFunc($elem['key'], $elem['value'], $deep, '+');
-                        break;
-                    default:
-                        $res .= $getFormattedStringFunc($elem['key'], $elem['value'], $deep);
-                }
-                return $acc . $res;
-            },
-            ''
-        )
-        . str_repeat(TAB, $deep) . '}' . PHP_EOL;
+    $offset = str_repeat(TAB, $deep);
+    $result = array_reduce(
+        $diff,
+        function ($acc, $elem) use ($deep, $offset) {
+            switch ($elem['changes']) {
+                case 'r':
+                    return [...$acc, $offset . getFormattedString($elem['key'], $elem['value'], $deep, '-')];
+                case 'a':
+                    return [...$acc, $offset . getFormattedString($elem['key'], $elem['value'], $deep, '+')];
+                case 'u':
+                    return [
+                            ...$acc,
+                            $offset . getFormattedString($elem['key'], $elem['oldValue'], $deep, '-'),
+                            $offset . getFormattedString($elem['key'], $elem['value'], $deep, '+')
+                        ];
+            }
+            return [...$acc, $offset . getFormattedString($elem['key'], $elem['value'], $deep)];
+        },
+        ['{']
+    );
+    return implode(PHP_EOL, $result) . PHP_EOL . $offset . '}';
 }
